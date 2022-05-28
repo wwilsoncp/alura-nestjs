690b12d3cd7dba945172f0306cf85e00db3e3ef7
Wilson Paiva <paiva.wilson@dedalus.com>
2022-05-28 16:23:35 -0300

    Adicionando configuração de conexão com o mysql e crud de livros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Livro } from './livro.model';
 import { LivrosController } from './livros.controller';
 import { LivrosService } from './livros.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql', // dialeto do banco de dados
+      host: 'localhost', // endereço do servidor do banco de dados
+      port: 3306, // porta do banco de dados. A porta padrão é 3306, porém se foi escolhida outra na instalação, alterar
+      username: 'root', // usuário do MySQL
+      password: 'mysqlwill', // senha do usuário do MySQL
+      database: 'livraria', // nome do banco de dados
+      autoLoadModels: true, // reconhecer automaticamente os modelos que foram anotados no projeto
+      synchronize: true, // quando subir a aplicação irá importar automaticamente os módulos adicionados no forFeature e vai sincronizar com o banco de dados
+    }),
+    SequelizeModule.forFeature([Livro]), // anotando os modelos
+  ],
   controllers: [AppController, LivrosController],
   providers: [AppService, LivrosService],
 })
